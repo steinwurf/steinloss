945e7a9db138adad42d6a82b5637f6e545781ea4
AndersSpringborg <aaspringborg@gmail.com>
2020-07-21 15:34:37 +0200

    packet entity compare function

--- src/packet_entity.py
+++ src/packet_entity.py
@@ -2,3 +2,8 @@ class Packet_entity:
     def __init__(self, id, time):
         self.id = id
         self.time = time
+
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.id == other.id and self.time == other.time
+        return False
