7b69956ef907129ffdd414a3c2470b8513a8c9fa
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-10-27 16:26:19 +0100

    putting dashboard and server together

--- steinloss.py
+++ steinloss.py
@@ -1,4 +1,9 @@
+import socket
 import sys
+import threading
+from threading import Thread
+
+from src.dashboard import dashboard
 from src.probe import Probe
 from src.server import Server
 
@@ -8,15 +13,14 @@ class TaskFactory:
     def create_task():
         first_arg = sys.argv[1] if len(sys.argv) > 1 else 'help'
         if first_arg == 's':
-            return Server.gigabyte()
+            return FrontEndAndBackEnd()
         elif first_arg == 'p':
-            return Probe(('192.168.0.107', 7070))
+            return Probe(('10.0.0.1', 9220))
         else:
             return Help()
 
 
 class Steinloss:
-
     @staticmethod
     def run():
         task = TaskFactory.create_task()
@@ -30,6 +34,18 @@ class Help(object):
         """
 
         print(help_msg)
+
+
+class FrontEndAndBackEnd:
+
+    def run(self):
+        server = Server()
+        t = Thread(target=server.run)
+        t.start()
+
+        dashboard.run()
+
+        t.join()
 
 
 Steinloss.run()
