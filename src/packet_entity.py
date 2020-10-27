366ac970d23bc26c7d5fa92a4dbb7aa4f74075d4
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-10-27 14:24:39 +0100

    Introduced two child classes to packet_entity.py. Namely sent_package and receive_package

--- src/packet_entity.py
+++ src/packet_entity.py
@@ -1,9 +1,29 @@
+from datetime import datetime
+
+
 class Packet_entity:
-    def __init__(self, packet_content, time):
+    def __init__(self, packet_content, time=datetime.now()):
         self.id = packet_content
         self.time = time
 
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return self.id == other.id and self.time == other.time
+            return self.id == other.id
         return False
+
+    def __repr__(self):
+        return f"id: {self.id} | time:  {self.time.strftime('%H:%M:%S')}"
+
+
+class sent_package(Packet_entity):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class receive_package(Packet_entity):
+    def __init__(self, sent_id, recv_id, *kwargs):
+        self.recv_id = recv_id
+        super().__init__(sent_id, *kwargs)
+
+    def __repr__(self):
+        return f"id:{self.id} recv:{self.recv_id} time:{self.time}"
