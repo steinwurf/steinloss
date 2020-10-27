a0d000649990622b674bc6b3dc396b173ae5257f
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-10-27 14:26:51 +0100

    refactored loss_calculator to have a time_table and package_label. These stores data about data loss

--- src/loss_calculator.py
+++ src/loss_calculator.py
@@ -1,11 +1,121 @@
-from src.packet_entity import Packet_entity
+from collections.abc import MutableMapping
+from datetime import datetime
+from itertools import takewhile
+
+from src.packet_entity import Packet_entity, receive_package, sent_package
+
+
+class TimeEntry:
+    def __init__(self):
+        self.loss = 0
+        self.sent = 0
+        self.received = 0
+
+    def add_packet(self, packet):
+        if type(packet) is sent_package:
+            self.sent += 1
+            self.loss += 1
+        elif type(packet) is receive_package:
+            self.received += 1
+
+    def __repr__(self):
+        return f"{type(self).__name__}: loss:{self.loss} - sent_package:{self.sent} received:{self.received}"
+
+
+class Time_table:
+    def __init__(self):
+        self.dict = dict()
+
+    def __getitem__(self, key) -> TimeEntry:
+        if isinstance(key, datetime):
+            time_key = self.convert_time_to_key(key)
+        else:
+            time_key = key
+        if time_key not in self.dict.keys():
+            self.dict[time_key] = TimeEntry()
+        return self.dict[time_key]
+
+    @staticmethod
+    def convert_time_to_key(packet_time: datetime):
+        return packet_time.strftime("%H:%M:%S")
+
+    def __repr__(self):
+        return f"{type(self).__name__}: {str(self.dict)}"
+
+    def __iter__(self):
+        return iter(self.dict)
+
+
+class packet_entry:
+    sent_at: datetime
+    received_at: datetime
+
+    def __init__(self):
+        self.sent_at = None
+        self.received_at = None
+
+    def is_sent(self):
+        return self.sent_at is not None
+
+    def is_received(self):
+        return self.received_at is not None
+
+    def __repr__(self):
+        return f"{type(self).__name__}: sent:{self.sent_at} → recv:{self.received_at}"
+
+
+class packet_table(dict):
+    def __getitem__(self, key) -> packet_entry:
+        if key not in self.keys():
+            self.__setitem__(key, packet_entry())
+        return super().__getitem__(key)
+
+    def __repr__(self):
+        return f"{type(self).__name__}: {super().__repr__()}"
+
+    def __iter__(self):
+        return super().__iter__()
 
 
 class Loss_Calculator:
-    packet_table = dict()
+    def __init__(self):
+        self.time_table = Time_table()
+        self.packet_table = packet_table()
 
     def add(self, packet: Packet_entity):
-        self.packet_table[packet.id] = packet
+        if type(packet) is sent_package:
+            self.packet_table[packet.id].sent_at = packet.time
+        elif type(packet) is receive_package:
+            self.packet_table[packet.id].received_at = packet.time
 
-    def __getitem__(self, item):
-        return self.packet_table[item]
+            sent_timestamp = self.packet_table[packet.id].sent_at
+            self.time_table[sent_timestamp].loss -= 1
+
+        self.time_table[packet.time].add_packet(packet)
+
+    def __contains__(self, item):
+        if isinstance(item, Packet_entity):
+            return item.id in self.packet_table
+        elif isinstance(item, datetime):
+            return item in self.time_table
+
+    def __getitem__(self, key):
+        if isinstance(key, datetime):
+            return self.time_table[key]
+        else:
+            return self.packet_table[key]
+
+    def __repr__(self):
+        return f"{str(self.time_table)}\n{str(self.packet_table)}"
+
+    def get_last_packages(self, number_of_packages: int) -> [packet_entry]:
+        arr = []
+        counter = number_of_packages
+        i = iter(reversed(self.packet_table))
+
+        while counter > 0:
+            key = next(i)
+            arr.append(self.packet_table[key])
+            counter -= 1
+
+        return arr
--- tests/test_loss_calculator.py
+++ tests/test_loss_calculator.py
@@ -1,16 +1,152 @@
-from datetime import datetime
+from freezegun import freeze_time
+from datetime import datetime, timedelta, time
 
-from src.loss_calculator import Loss_Calculator
-from src.packet_entity import Packet_entity
+from src.loss_calculator import Loss_Calculator, Time_table
+from src.packet_entity import Packet_entity, sent_package, receive_package
 
 
 class Test_Loss_Calculator:
     def test_initiate_loss_calculator(self):
         Loss_Calculator()
 
-    def test_loss_calculator_can_add_packet_entry(self):
+    def test_loss_calculator_can_add_packet_entry(self, freezer):
+        loss_calculator = Loss_Calculator()
+        packet = sent_package('1', datetime.now())
+
+        loss_calculator.add(packet)
+
+        assert packet in loss_calculator
+
+    @freeze_time("00:00:00", auto_tick_seconds=1)
+    def test_two_entries_with_more_than_a_second_apart_should_not_be_in_same_key(self, freezer):
+        loss_calculator = Loss_Calculator()
+        first_time = datetime.now()
+        second_time = datetime.now()
+
+        packet_one = sent_package('1', first_time)
+        packet_two = sent_package('2', second_time)
+        loss_calculator.add(packet_one)
+        loss_calculator.add(packet_two)
+
+        assert loss_calculator[first_time].sent == 1
+        assert loss_calculator[second_time].sent == 1
+
+    @freeze_time("00:00:00", auto_tick_seconds=0.5)
+    def test_loss_calculator_can_add_two_entries_at_same_timestamp(self, freezer):
+        loss_calculator = Loss_Calculator()
+        at_zero_second = datetime.now()
+        at_zero_point_5_second = datetime.now()
+        packet_at_zero_second = sent_package('1', at_zero_second)
+        packet_at_zero_point_5_second = sent_package('2', at_zero_point_5_second)
+
+        loss_calculator.add(packet_at_zero_second)
+        loss_calculator.add(packet_at_zero_point_5_second)
+
+        assert loss_calculator[at_zero_second].sent == 2
+
+    def test_loss_calculator_counts_sent_packages_at_timestamp(self):
+        loss_calculator = Loss_Calculator()
+        time_stamp = datetime.now()
+
+        loss_calculator.add(sent_package('1', time_stamp))
+        loss_calculator.add(sent_package('2', time_stamp))
+
+        assert loss_calculator[time_stamp].sent == 2
+
+    def test_loss_calculator_counts_received_packages_at_timestamp(self):
+        loss_calculator = Loss_Calculator()
+        time_stamp = datetime.now()
+
+        loss_calculator.add(sent_package('1', time_stamp))
+        loss_calculator.add(receive_package('1', '1', time_stamp))
+
+        assert loss_calculator[time_stamp].received == 1
+
+    def test_loss_calculator_counts_loss_when_packet_do_not_return(self):
+        loss_calculator = Loss_Calculator()
+        time_stamp = datetime.now()
+
+        loss_calculator.add(sent_package('1', time_stamp))
+        loss_calculator.add(sent_package('2', time_stamp))
+        loss_calculator.add(sent_package('3', time_stamp))
+
+        loss_calculator.add(receive_package('1', '1', time_stamp))
+        loss_calculator.add(receive_package('3', '2', time_stamp))
+
+        assert loss_calculator[time_stamp].loss == 1
+
+    def test_sent_package_added_to_calculator_is_logged(self):
+        loss_calculator = Loss_Calculator()
+        time_stamp = datetime.now()
+        package = sent_package('1', time_stamp)
+
+        loss_calculator.add(package)
+
+        assert loss_calculator[package.id].is_sent()
+
+    def test_sent_package_added_to_calculator_is_not_received(self):
+        loss_calculator = Loss_Calculator()
+        time_stamp = datetime.now()
+        package = sent_package('1', time_stamp)
+
+        loss_calculator.add(package)
+
+        assert not loss_calculator[package.id].is_received()
+
+    @freeze_time("00:00:00")
+    def test_convert_datetime_max_value_for_rounding_down(self):
+        time_table = Time_table()
+
+        # 1000 milliseconds = 1 second
+        at_999_milliseconds = datetime.now() + timedelta(milliseconds=999)
+        at_0_second_string = time_table.convert_time_to_key(at_999_milliseconds)
+
+        assert at_0_second_string == "00:00:00"
+
+    @freeze_time("00:00:00")
+    def test_convert_datetime_returns_seconds_in_string(self):
+        time_table = Time_table()
+
+        # 1000 milliseconds = 1 second
+        at_1_milliseconds = datetime.now() + timedelta(seconds=1)
+        at_1_second_string = time_table.convert_time_to_key(at_1_milliseconds)
+
+        assert at_1_second_string == "00:00:01"
+
+    def test_get_last_package(self):
         loss_calculator = Loss_Calculator()
 
-        loss_calculator.add(Packet_entity('1', datetime.now()))
+        package = sent_package("1")
+        loss_calculator.add(package)
 
-        assert loss_calculator['1'] is not None
+        last_package = loss_calculator.get_last_packages(1).pop()
+        assert last_package.sent_at == package.time
+
+    def test_get_n_latest_packages_where_n_is_equal_to_length(self):
+        loss_calculator = Loss_Calculator()
+        loss_calculator.add(sent_package("1"))
+        loss_calculator.add(sent_package("2"))
+
+        packages = loss_calculator.get_last_packages(2)
+
+        assert len(packages) == 2
+
+    @freeze_time(auto_tick_seconds=1)
+    def test_get_n_latest_packages(self):
+        loss_calculator = Loss_Calculator()
+        p1 = sent_package("1", datetime.now())
+        p2 = sent_package("2")
+        p3 = sent_package("3")
+
+        loss_calculator.add(p1)
+        loss_calculator.add(p2)
+        loss_calculator.add(p3)
+
+        packages = loss_calculator.get_last_packages(2)
+
+        assert not any(pack.sent_at == p1.time for pack in packages)
+        assert any(pack.sent_at == p2.time for pack in packages)
+        assert any(pack.sent_at == p3.time for pack in packages)
+
+    # Test ideas:
+    # what happens after 24H
