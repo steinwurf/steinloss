d5988633224bcf3ef6944477682e28390f578eab
Anders Aaen Springborg <aaspringborg@gmail.com>
2021-01-27 10:49:41 +0100

    back to old ip system

--- steinloss/server.py
+++ steinloss/server.py
@@ -167,9 +167,16 @@ class Server:
         loop.close()
 
     def get_local_ip(self):
-        hostname = socket.gethostname()
-        local_ip = socket.gethostbyname(hostname)
-        return local_ip
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('8.8.8.8', 1))
+            ip = s.getsockname()[0]
+        except Exception:
+            ip = '127.0.0.1'
+        finally:
+            s.close()
+        return ip
 
 
 class EchoServerProtocol(asyncio.DatagramProtocol):
