15f62a63edb3bf0efd315b4f1dd63878f10a37ac
jaxels20 <xfm82gtn@gmail.com>
2022-03-27 19:13:38 +0200

    fixed lints

--- tests/test_Server.py
+++ tests/test_Server.py
@@ -10,7 +10,7 @@ MEGABYTE = 1024 * KILOBYTE
 class TestServer:
     @pytest.mark.parametrize(
         "speed, batchsize, interval",
-        [(KILOBYTE, 1, 1), (4 * MEGABYTE, 100, 0.0244140625), (8 * MEGABYTE,50, 0.006103515625)]
+        [(KILOBYTE, 1, 1), (4 * MEGABYTE, 100, 0.0244140625), (8 * MEGABYTE, 50, 0.006103515625)]
     )
     def test_two_kilobytes_should_set_interval_to_half(self, speed, batchsize, interval):
         server = Server(speed=speed, batch_size=batchsize)
