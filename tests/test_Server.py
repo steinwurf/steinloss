f547a721b95859a02f27a9ae9f55150faecbb61f
jaxels20 <xfm82gtn@gmail.com>
2022-03-27 19:06:52 +0200

    fixed lints

--- tests/test_Server.py
+++ tests/test_Server.py
@@ -8,14 +8,13 @@ MEGABYTE = 1024 * KILOBYTE
 
 
 class TestServer:
-    
     @pytest.mark.parametrize(
-        "speed, batchsize, interval", [(KILOBYTE, 1, 1), (4 * MEGABYTE, 100, 0.0244140625), (8 * MEGABYTE, 50, 0.006103515625)]
+        "speed, batchsize, interval", [(KILOBYTE, 1, 1), (4 * MEGABYTE, 100, 0.0244140625), (8 * MEGABYTE,
+        50, 0.006103515625)]
     )
     def test_two_kilobytes_should_set_interval_to_half(self, speed, batchsize, interval):
         server = Server(speed=speed, batch_size=batchsize)
         assert server.interval == interval
-
 
     def test_wait_for_probe_should_return_address_of_probe(self, mocker):
         server = Server()
@@ -23,3 +22,4 @@ class TestServer:
         mocker.patch('socket.socket.recvfrom', return_value=[b"package", fake_address])
 
         assert server.wait_for_probe() == fake_address
+        
